1d1110c55e0aadac33c021b05c6d67a931019d03
Shekar Reddy <getbetterid@gmail.com>
2025-12-12 20:02:45 +0530

    cleanup

--- pullmatchingou.ps1
+++
@@ -1,9 +0,0 @@
-# Prefix to search for
-$prefix = "CA10"
-
-# Distinguished Name of the CLD OU
-$parentOU = "OU=CLD,DC=yourdomain,DC=com"
-
-# Get immediate child OUs under CLD and filter by prefix
-Get-ADOrganizationalUnit -SearchBase $parentOU -SearchScope OneLevel -Filter "Name -like '$prefix*'" |
-    Select-Object Name, DistinguishedName
